8c8b730318b633a8e4c2ad620ad996b0c26ed02d
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-04-13 09:39:09 +0300

    incomplete context lab

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
+function createEmployeeRecord(arr){
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+function createEmployeeRecords(){
+    return employeeDataArray.map(createEmployeeRecord);
+}
+  // Function to add a timeIn event to an employee's record
+  function createTimeInEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+  }
+  // Function to add a timeOut event to an employee's record
+  function createTimeOutEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+  }
+   // Function to calculate the hours worked on a specific date
+   function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+  }
+  // Function to calculate wages earned on a specific date
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
 
+   // Function to calculate payroll for all employees
+   function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => {
+      return totalPayroll + allWagesFor.call(employee);
+    }, 0);
+  }
+  
+  // Function to find an employee record by first name in a collection
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+  }
+  // Exporting functions
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll
+  };
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
